f23a14253a07d5dfe1e533e14799425d95b2b4ad
S <sxa1705@student.bham.ac.uk>
2025-11-19 17:05:00 +0000

    Integrate TTS for object detection and wall warnings
    Added TTS feedback for detected objects and wall proximity.

--- controllers/text_to_speech.py
+++ controllers/text_to_speech.py
@@ -11,6 +11,7 @@ robot = Robot()
 # Initialisation TTS module
 engine = pyttsx3.init()
 engine.setProperty("rate",180)
+engine.setProperty('volume', 0.9)
 
 def speak(audio):
     engine.say(audio)
@@ -26,8 +27,9 @@ def tts_from_yolo(detected_obj):
     speak(feedback)
 
 last_feedback_obj= None
+last_wall_warning= False
 
-speak("Simulation started")
+engine.say("Simulation started")
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -52,3 +54,37 @@ while robot.step(timestep) != -1:
     pass
 
 # Enter here exit cleanup code.
+
+
+#code ADDED into yolo controller to send audio feedback when object + wall is detected 
+ # Step 7: Integrate with the TTS module 
+            # Audio Feedback and Retrieving Information on Detected Objects
+            boxes = r.boxes
+            for box in boxes:
+                cls_id = int(box.cls[0]) # Get category ID
+                cls_name = model.names[cls_id] # Get category names
+                
+                # Trigger the TTS module
+                # such: print(f"find: {cls_name}")
+                # Then call: tts_module.warn(cls_name)
+                # If statment used to not allow overlapping + repatitive warning
+                current_obj = ", ".join(detected_objects)
+                
+                if current_obj != last_feedback_obj:
+                    tts_from_yolo(detected_objects)
+                    last_feedback_obj = current_obj
+                    last_wall_warning = False 
+
+        # Step8 Wall Inspection
+        # Reading distance
+        distance = distance_sensor.getValue()
+        # Debug
+        #print("distance:", distance)
+
+        # Distance less than 0.5 and YOLO fails to detect objects → Classified as a wall
+        if distance < 0.5 and len(detected_objects) == 0:
+            if not last_wall_warning:
+                print("Wall ")
+                engine.say("Wall ahead avoid hitting it")
+                last_wall_warning = True
+                last_feedback_obj = None
