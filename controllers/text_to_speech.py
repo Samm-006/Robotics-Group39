fcdf1bde69cd7d5ff98b33d190af21008fc2a793
S <sxa1705@student.bham.ac.uk>
2025-11-28 00:23:12 +0000

    Object avoidance

--- controllers/text_to_speech.py
+++ controllers/text_to_speech.py
@@ -2,6 +2,8 @@
 
 from controller import Robot, Keyboard 
 import pyttsx3 # For audio feedback
+import random 
+import time
 
 # create the Robot instance.
 robot = Robot()
@@ -15,6 +17,10 @@ right_motor= robot.getDevice("right wheel motor")
 left_motor.setPosition(float("inf"))
 right_motor.setPosition(float("inf"))
 
+#Lidar
+lidar = robot.getDevice('LDS-01')
+lidar.enable(timestep)
+lidar.enablePointCloud()
 
 # Initialisation of keyboard for movement 
 keyboard = Keyboard()
@@ -25,27 +31,24 @@ engine = pyttsx3.init()
 engine.setProperty("rate",180)
 engine.setProperty('volume', 0.9)
 
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-   
-   
-def tts_from_yolo(detected_obj):
-    if not detected_obj:
-        return
-        
-    label = ", ".join(detected_obj)
-    feedback = f"{label} ahead, avoid hitting it."
-    speak(feedback)
-
 
 engine.say("Simulation started")
 
+# Object avoidance Params
+CRUISE_SPEED = 3.0   
+TURN_SPEED = 0.5     
+OBSTACLE_DIST = 0.8 
+    
+robot_state = 'FORWARD'
+state_timer = 0
+
+last_detected_obj = "None"
+seen_objects = set()
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    
+    '''    
     # Setup Arrows to move robot (↑, ↓, ←, →) 
     key=keyboard.getKey()
     
@@ -71,7 +74,50 @@ while robot.step(timestep) != -1:
     
     left_motor.setVelocity(left_wheel_speed)
     right_motor.setVelocity(right_wheel_speed)
+'''
+    left_wheel_speed = 0.0
+    right_wheel_speed = 0.0
+    speed = 2.5
     
+    lidar_data = lidar.getRangeImage()
+    if not lidar_data: 
+        continue
+
+    front_idx = len(lidar_data) // 2
+    start_idx = max(0, front_idx - 40)
+    end_idx = min(len(lidar_data), front_idx + 40)
+    front_sector = lidar_data[start_idx : end_idx]
+    valid_readings = [d for d in front_sector if d != float('inf')]
+    min_front = min(valid_readings) if valid_readings else 10.0
+   
+    if robot_state == 'FORWARD':
+        if min_front < OBSTACLE_DIST:
+            robot_state = 'REVERSE'
+            state_timer = 60 
+        else:
+            left_motor.setVelocity(CRUISE_SPEED)
+            right_motor.setVelocity(CRUISE_SPEED)
+    elif robot_state == 'REVERSE':
+        if state_timer > 0:
+            left_motor.setVelocity(-1.0)
+            right_motor.setVelocity(-1.0)
+            state_timer -= 1
+        else:
+            robot_state = 'TURN'
+            state_timer = 60 
+            direction = 1 if random.random() > 0.5 else -1
+            current_turn_velocity = TURN_SPEED * direction
+    elif robot_state == 'TURN':
+        if state_timer > 0:
+            left_motor.setVelocity(-current_turn_velocity)
+            right_motor.setVelocity(current_turn_velocity)
+            state_timer -= 1
+        else:
+            if min_front > OBSTACLE_DIST + 0.1:
+                robot_state = 'FORWARD'
+            else:
+                state_timer = 30        
+         
     
 
             # Step 7: Integrate with the TTS module 
@@ -84,10 +130,17 @@ while robot.step(timestep) != -1:
                 # Trigger the TTS module
                 # such: print(f"find: {cls_name}")
                 # Then call: tts_module.warn(cls_name)
-                # If statment used to not allow overlapping + repatitive warning
-                tts_from_yolo(detected_objects)
-
-    # Step8 Wall Inspection
+                 
+                # Save last object detected
+                last_detected_obj = cls_name 
+                 
+                # TTS Voice output for new object
+                if float(box.conf[0]) > 0.5 and cls_name not in seen_objects:
+                    engine.say(f"{cls_name} ahead avoid hitting it")
+                    engine.runAndWait()
+                    seen_objects.add(cls_name)
+                    
+# Step 8: Wall Inspection
         # Reading distance
         distance = distance_sensor.getValue()
 
@@ -96,7 +149,7 @@ while robot.step(timestep) != -1:
 
         # Distance less than 0.5 and YOLO fails to detect objects → Classified as a wall
         if distance < 0.5 and len(detected_objects) == 0:
-           print("Wall ")
-           engine.say("Wall ahead avoid hitting it")
-         
-         
+            print("Wall")
+            engine.say("Wall ahead avoid hitting it")
+            engine.runAndWait()
+        
